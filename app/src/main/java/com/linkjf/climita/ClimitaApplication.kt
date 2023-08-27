954ca4a60c4fb7737e3afaa41c943bf3acb1eff7
Jose Ortega Garcia <link.josef@gmail.com>
2023-08-26 19:51:06 -0500

    setup DI

---
+++ app/src/main/java/com/linkjf/climita/ClimitaApplication.kt
@@ -0,0 +1,8 @@
+package com.linkjf.climita
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ClimitaApplication : Application() {
+}
--- app/src/main/java/com/linkjf/climita/presentation/MainActivity.kt
+++ app/src/main/java/com/linkjf/climita/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.linkjf.climita
+package com.linkjf.climita.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.linkjf.climita.ui.theme.ClimitaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
